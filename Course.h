a19bdb99fc942a216fe692835967a69e0db48c99
OmarOgaiz <omar.ogaiz@student.giu-uni.de>
2025-11-11 10:15:43 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -5,6 +5,25 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
+class Course {
+private:
+    string coursename;
+    string coursecode;
+    int Maxstudents;
+    Student* students;
+    int currentstudent;
+public:
+    Course(string cn, string cc, int ms) {
+        coursename = cn;
+        coursecode = cc;
+        Maxstudents = ms;
+        students = new Student[Maxstudents];
+        currentstudent = 0;
+    }
+
+    ~Course() {
+        delete[] students;
+    }
 
 
 

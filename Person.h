ecc0413c2865d79c72a29a0df9e28e5a95ca2d76
OmarOgaiz <omar.ogaiz@student.giu-uni.de>
2025-11-11 10:20:28 +0200

    Add Person class with constructor and display method

--- Person.h
+++ Person.h
@@ -3,7 +3,20 @@
 
 #include <string>
 using namespace std;
+class Person {
+private:
+    string name;
+    int id;
+public:
+    Person(string n, int i) {
+        name = n;
+        id = i;
+    }
 
+    void display() {
+        cout << "Name: " << name << ", ID: " << id << endl;
+    }
+};
 //#write Person class here
 
 

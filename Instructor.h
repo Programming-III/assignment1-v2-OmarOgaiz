5a9d928077be54bf317ed5c50effd07f6019e307
OmarOgaiz <omar.ogaiz@student.giu-uni.de>
2025-11-11 10:20:03 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -4,6 +4,21 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
+class Instructor : public Person {
+private:
+    string department;
+    int experienceyears;
+public:
+    Instructor(string n, int i, string d, int e) : Person(n, i) {
+        department = d;
+        experienceyears = e;
+    }
+
+    void display() {
+        Person::display();
+        cout << "Department: " << department << ", Experience Years: " << experienceyears << endl;
+    }
+};
 
 
 
